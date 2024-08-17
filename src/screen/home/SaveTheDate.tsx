c5f03e5d2df4d5576f8aba658b53ea9744f798a4
Manh ND <manh16nd@proton.me>
2024-08-17 22:39:43 +0700

    build

--- src/screen/home/SaveTheDate.tsx
+++ src/screen/home/SaveTheDate.tsx
@@ -8,11 +8,18 @@ export const SaveTheDate = () => {
                 <div className="text-xl font-thin">
                     Đến dự buổi tiệc chung vui của chúng tôi tại
                 </div>
-                <div className=" font-['Hurricane'] text-xl md:text-8xl my-4 md:my-8">
+                <div className=" font-['Hurricane'] text-2xl md:text-8xl my-4 md:my-8">
                     W.Jardin
                 </div>
                 <div className="text-lg">307 Nguyễn Văn Linh</div>
                 <div className="text-lg">Dư Hàng Kênh, Lê Chân, Hải Phòng</div>
+                <div className="pt-8" />
+                <div className="text-2xl md:text-6xl">22 . 09 . 2024</div>
+                <div className="pt-8" />
+                <div className="text-xl font-thin">
+                    Sự hiện diện của quý khách là niềm vinh hạnh cho gia đình
+                    chúng tôi.
+                </div>
             </div>
 
             <div className="pt-4 md:pt-16" />
