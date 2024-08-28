c1d6ba6d8bd5fa2236172651e4d63d15ade3dccb
Manh ND <manh16nd@proton.me>
2024-08-28 23:36:06 +0700

    build

--- src/screen/home/Head.tsx
+++ src/screen/home/Head.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { IoHeart } from 'react-icons/io5'
-import ScrollAnimation from 'react-animate-on-scroll'
 
 export const HomeHead = () => {
-    const images = ['/img/manhlinh-banner-01.JPG']
+    const images = [
+        '/img/manhlinh-banner-01.JPG',
+        '/img/manhlinh-banner-02.JPG',
+    ]
 
     const [img] = useState(images[0])
 
@@ -11,40 +13,62 @@ export const HomeHead = () => {
         <div>
             <div className="h-screen">
                 <div
-                    className="h-full transition-all"
+                    className="h-full transition-all block md:hidden"
                     style={{
                         background: `url(${img})`,
                         backgroundSize: 'cover',
                         backgroundPosition: 'center 20%',
                     }}
                 >
-                    <ScrollAnimation
-                        animateIn="bounceInUp"
-                        initiallyVisible
-                        animateOnce
-                        delay={450}
-                    >
-                        <div className="relative w-full h-full flex flex-wrap items-start justify-center pt-16 md:pt-32">
-                            <div className="text-center font-viao text-white">
-                                <div className="text-2xl font-bold font-['Montserrat']">
-                                    SAVE THE DATE
-                                </div>
-                                <div className="pt-4 md:pt-8" />
-                                <div className="text-4xl md:text-6xl flex">
-                                    Đức Mạnh <span className="ml-1 md:ml-4" />
-                                    <IoHeart color="white" />
-                                    <span className="mr-1 md:mr-3" />
-                                    Thùy Linh
-                                </div>
-                                <div className="pt-4 md:pt-8" />
-                                <div className=" px-4 md:px-0">
-                                    <div className="font-viao font-semibold font-['Montserrat'] text-3xl md:text-4xl px-8 py-2 md:py-4 border-2 border-white shadow-2xl focus:bg-white focus:text-transparent active:bg-white active:text-border-theme-main transition-all">
-                                        22.09.2024
-                                    </div>
+                    <div className="relative w-full h-full flex flex-wrap items-start md:items-end justify-center pt-16 md:p-0">
+                        <div className="text-center font-viao text-white md:text-theme-main">
+                            <div className="text-2xl font-bold font-['Montserrat']">
+                                SAVE THE DATE
+                            </div>
+                            <div className="pt-4 md:pt-8" />
+                            <div className="text-4xl md:text-6xl flex">
+                                Đức Mạnh <span className="ml-1 md:ml-4" />
+                                <IoHeart color="white" />
+                                <span className="mr-1 md:mr-3" />
+                                Thùy Linh
+                            </div>
+                            <div className="pt-4 md:pt-8" />
+                            <div className=" px-4 md:px-0">
+                                <div className="font-viao font-semibold font-['Montserrat'] text-3xl md:text-4xl px-8 py-2 md:py-4 border-2 border-white shadow-2xl focus:bg-white focus:text-transparent active:bg-white active:text-border-theme-main transition-all">
+                                    22.09.2024
                                 </div>
                             </div>
                         </div>
-                    </ScrollAnimation>
+                    </div>
+                </div>
+                <div
+                    className="h-full transition-all hidden md:block"
+                    style={{
+                        background: `url(${images[1]})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center 40%',
+                    }}
+                >
+                    <div className="relative w-full h-full flex flex-wrap items-end justify-center pb-16">
+                        <div className="text-center font-viao text-theme-bg">
+                            <div className="text-2xl font-bold font-['Montserrat']">
+                                SAVE THE DATE
+                            </div>
+                            <div className="pt-4 md:pt-8" />
+                            <div className="text-4xl md:text-6xl flex">
+                                Đức Mạnh <span className="ml-1 md:ml-4" />
+                                <IoHeart color="#f9f0e3" />
+                                <span className="mr-1 md:mr-3" />
+                                Thùy Linh
+                            </div>
+                            <div className="pt-4 md:pt-8" />
+                            <div className=" px-4 md:px-0">
+                                <div className="font-viao font-semibold font-['Montserrat'] text-3xl md:text-4xl px-8 py-2 md:py-4 border-2 border-theme-bg shadow-2xl focus:bg-white focus:text-transparent active:bg-white active:text-border-theme-main transition-all">
+                                    22.09.2024
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
