d4419a4f0ea72a13fb42d9d10ddb998692a85086
Manh ND <manh16nd@proton.me>
2024-09-01 05:26:20 +0700

    build

--- src/screen/home/Home.screen.tsx
+++ src/screen/home/Home.screen.tsx
@@ -40,7 +40,7 @@ export const HomeScreen = () => {
     return (
         <main
             // ref={el}
-            className="min-h-screen page bg-theme-bg"
+            className="min-h-screen overflow-x-hidden page bg-theme-bg"
         >
             {!init && <Loading show={init < 1} />}
             <HomeHead setInit={initOk} />
