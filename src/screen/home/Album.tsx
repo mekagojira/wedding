bbf42ae1050162015b844636a35bcfa52a554824
Manh ND <manh16nd@proton.me>
2024-09-01 18:23:50 +0700

    build

--- src/screen/home/Album.tsx
+++ src/screen/home/Album.tsx
@@ -1,8 +1,9 @@
 import { Section } from 'component/section/Section.tsx'
 import ImageGallery from 'react-image-gallery'
 import 'react-image-gallery/styles/scss/image-gallery.scss'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Divider } from 'component/section/Divider.tsx'
+import { IoIosClose } from 'react-icons/io'
 
 const imgs = [
     '/gallery/MAX06073-min.jpg',
@@ -20,33 +21,59 @@ const imgs = [
 
 export function Album() {
     const gallery = useRef<ImageGallery>(null)
+    const [f, setF] = useState<boolean>(false)
 
-    const fullscreen = () =>
+    useEffect(() => {
+        const state = gallery?.current?.state as {
+            isFullscreen: boolean
+        } | null
+        console.log({ state })
+
+        if (state?.isFullscreen != null) setF(state.isFullscreen)
+    }, [gallery?.current?.state])
+
+    const fullscreen = () => {
         gallery?.current?.fullScreen && gallery.current.fullScreen()
+        setF(true)
+    }
 
     return (
-        <Section>
-            <div className="flex items-center flex-col">
-                <div className="pt-2" />
+        <>
+            {f && (
                 <div
-                    onClick={fullscreen}
-                    className="font-viao text-2xl text-center uppercase shadow-lg font-black bg-theme-main text-theme-bg px-3 py-2"
+                    className="fixed top-0 cursor-pointer right-0 z-50 p-4"
+                    onClick={() => {
+                        setF(false)
+                        gallery?.current?.exitFullScreen &&
+                            gallery.current.exitFullScreen()
+                    }}
                 >
-                    Album cưới
+                    <IoIosClose color={'white'} size={32} />
                 </div>
-                <div className="pt-4" />
-                <div className="mx-auto container">
-                    <ImageGallery
-                        items={imgs}
-                        ref={gallery}
+            )}
+            <Section>
+                <div className="flex items-center flex-col">
+                    <div className="pt-2" />
+                    <div
                         onClick={fullscreen}
-                        useBrowserFullscreen={false}
-                    />
+                        className="font-viao text-2xl text-center uppercase shadow-lg font-black bg-theme-main text-theme-bg px-3 py-2"
+                    >
+                        Album cưới
+                    </div>
+                    <div className="pt-4" />
+                    <div className="mx-auto container">
+                        <ImageGallery
+                            items={imgs}
+                            ref={gallery}
+                            onClick={fullscreen}
+                            useBrowserFullscreen={false}
+                        />
+                    </div>
+                    <div className="py-8">
+                        <Divider />
+                    </div>
                 </div>
-                <div className="py-8">
-                    <Divider />
-                </div>
-            </div>
-        </Section>
+            </Section>
+        </>
     )
 }
