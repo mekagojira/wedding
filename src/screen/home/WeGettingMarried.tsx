bb924a9933815e6a519255b4e877dc624a12aed5
Manh ND <manh16nd@proton.me>
2024-09-01 12:17:30 +0700

    build

--- src/screen/home/WeGettingMarried.tsx
+++ src/screen/home/WeGettingMarried.tsx
@@ -12,6 +12,19 @@ export const WeGettingMarried = () => {
         setGuest(search.get('guest') || search.get('g') || '')
     }, [])
 
+    const heart = (left: boolean) => (
+        <>
+            <div className={`${left ? '-rotate-45' : 'rotate-45'}`}>
+                <div className="block md:hidden">
+                    <HiHeart size={24} />
+                </div>
+                <div className="hidden md:block">
+                    <HiHeart size={42} />
+                </div>
+            </div>
+        </>
+    )
+
     return (
         <>
             <Section>
@@ -19,14 +32,10 @@ export const WeGettingMarried = () => {
                     <Divider r={true} />
                 </div>
                 <div className="flex flex-col items-center">
-                    <div className="text-center text-theme-main font-viao text-2xl font-bold flex pb-4">
-                        <div className="-rotate-45">
-                            <HiHeart size={24} />
-                        </div>
+                    <div className="text-center text-theme-main font-viao text-2xl md:text-5xl font-bold flex pb-4">
+                        {heart(true)}
                         <span className="">We are getting married</span>
-                        <div className="rotate-45">
-                            <HiHeart size={24} />
-                        </div>
+                        {heart(false)}
                     </div>
                     <div className="flex p-4 justify-center items-center">
                         <img
@@ -35,7 +44,7 @@ export const WeGettingMarried = () => {
                             className="shadow-lg rounded md:w-1/2 shadow-xl"
                         />
                     </div>
-                    <div className="text-center text-theme-main font-['Corinthia'] italic text-5xl py-8">
+                    <div className="text-center text-theme-main font-['Corinthia'] italic text-5xl md:text-7xl py-8">
                         Đức Mạnh -Thùy Linh
                     </div>
                     <div className="container mx-auto px-4 md:px-0 flex items-center justify-center">
@@ -50,7 +59,7 @@ export const WeGettingMarried = () => {
                                     ease: 'linear',
                                 }}
                             >
-                                <div className="box-e md:p-8 text-justify text-theme-second font-viao text-lg">
+                                <div className="box-e md:p-8 text-justify text-theme-second font-viao text-lg md:text-xl">
                                     Sau hơn một thập kỷ đồng hành, cuối cùng đã
                                     đến ngày chúng mình trở thành một gia đình.
                                     Trân trọng mời {guest || 'bạn'} đến chung
